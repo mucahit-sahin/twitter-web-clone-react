bb899e477b7b82ca12ce8bdf936f33fcaa01f42c
mucahit-sahin <mucahitsahin786@gmail.com>
2021-01-27 22:29:04 +0300

    kullanıcının giriş yapmadan önceki ilk index ekranı tasarlandı

---
+++ src/pages/SignIndex/SignIndex.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ChatIcon from "../../components/icons/ChatIcon";
+import Logo from "../../components/icons/Logo";
+import SearchIcon from "../../components/icons/SearchIcon";
+import UsersIcon from "../../components/icons/UsersIcon";
+import "./style.css";
+
+function SignIndex() {
+  return (
+    <div className="container">
+      <div className="col1">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="info">
+          <div className="infoSearchRow">
+            <SearchIcon width={23} stroke="white" fill="white" />
+            <span className="infoText"> İlgi alanlarını takip et.</span>
+          </div>
+          <div className="infoUsersRow">
+            <UsersIcon width={23} stroke="white" fill="white" />
+            <span className="infoText">
+              İnsanların neler hakkında konuştuğuna kulak ver.
+            </span>
+          </div>
+          <div className="infoChatRow">
+            <ChatIcon width={23} stroke="white" fill="white" />
+            <span className="infoText"> Sohbete katıl.</span>
+          </div>
+        </div>
+      </div>
+      <div className="col2">
+        <div className="menu">
+          <Logo width={30} fill="white" />
+          <span className="header">Dünyada şu anda olup bitenleri gör</span>
+          <div className="joinRow">
+            <span className="join">Twitter'a bugün katıl.</span>
+          </div>
+          <div className="buttons">
+            <button href="" className="signup">
+              <div className="signupItem">
+                <span className="signupText">Üye ol</span>
+              </div>
+            </button>
+            <button href="" className="login">
+              <div className="loginItem">
+                <span className="loginText">Giriş yap</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIndex;
