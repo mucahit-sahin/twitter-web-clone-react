5947bb91138640c78307ec3cdc2c321404addcbd
mucahit-sahin <mucahitsahin786@gmail.com>
2021-02-02 13:15:03 +0300

    route a signup sayfası eklendi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import SignIndex from "./pages/SignIndex/SignIndex";
 import Login from "./pages/Login/Login";
 import { Route, Switch } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={SignIndex} exact />
       <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </Switch>
   );
 }
