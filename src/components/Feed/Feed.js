bfbd6036cc2f5cd22dc56fa8092593a298dc0dc4
mucahit-sahin <mucahitsahin786@gmail.com>
2021-02-05 23:43:11 +0300

    Feed header tasarlandı

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import "./Feed.css";
+import TweetBox from "./TweetBox/TweetBox";
+import Post from "./Post/Post";
+import ProfilePhoto from "../../assets/profile.png";
+import HomeStars from "../icons/HomeStars";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed-header">
+        <div className="feed-headerText">
+          <span>Home</span>
+        </div>
+        <div className="homeStarsCol">
+          <HomeStars className="homeStars" width={22} height={22} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
