db02eaa3ffced5283a38ad68c0d2afc6c95f4852
mucahit-sahin <mucahitsahin786@gmail.com>
2021-02-08 23:58:45 +0300

    Post ikonları oluşturuldu

---
+++ src/components/icons/RetweetIcon.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+function RetweetIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      className="prefix__r-4qtqp9 prefix__r-yyyyoo prefix__r-1xvli5t prefix__r-dnmrzs prefix__r-bnwqim prefix__r-1plcrui prefix__r-lrvibr prefix__r-1hdv0qi"
+      {...props}
+    >
+      <path d="M23.77 15.67a.749.749 0 00-1.06 0l-2.22 2.22V7.65a3.755 3.755 0 00-3.75-3.75h-5.85a.75.75 0 000 1.5h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22a.749.749 0 10-1.06 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5a.747.747 0 000-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22a.752.752 0 001.062 0 .749.749 0 000-1.06l-3.5-3.5a.747.747 0 00-1.06 0l-3.5 3.5a.749.749 0 101.06 1.06l2.22-2.22V16.7a3.755 3.755 0 003.75 3.75h5.85a.75.75 0 000-1.5z" />
+    </svg>
+  );
+}
+
+export default RetweetIcon;
---
+++ src/components/icons/SharePostIcon.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+function SharePostIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      className="prefix__r-4qtqp9 prefix__r-yyyyoo prefix__r-1xvli5t prefix__r-dnmrzs prefix__r-bnwqim prefix__r-1plcrui prefix__r-lrvibr prefix__r-1hdv0qi"
+      {...props}
+    >
+      <path d="M17.53 7.47l-5-5a.749.749 0 00-1.06 0l-5 5a.749.749 0 101.06 1.06l3.72-3.72V15a.75.75 0 001.5 0V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22a.749.749 0 000-1.06z" />
+      <path d="M19.708 21.944H4.292A2.294 2.294 0 012 19.652V14a.75.75 0 011.5 0v5.652c0 .437.355.792.792.792h15.416a.793.793 0 00.792-.792V14a.75.75 0 011.5 0v5.652a2.294 2.294 0 01-2.292 2.292z" />
+    </svg>
+  );
+}
+
+export default SharePostIcon;
