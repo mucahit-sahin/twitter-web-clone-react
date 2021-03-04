c3b2baa4210f82de95889a36a01ae0c20013a6d8
mucahit-sahin <mucahitsahin786@gmail.com>
2021-03-04 14:29:29 +0300

    ChatInputs focust stili ayarlandı

--- src/components/ChatInputs/ChatInputs.js
+++ src/components/ChatInputs/ChatInputs.js
@@ -3,12 +3,22 @@ import { EmojiIcon, GifIcon, PhotoIcon, SendIcon } from "../icons";
 import "./ChatInputs.css";
 
 const ChatInputs = () => {
+  const [isFocus, setIsFocus] = React.useState(false);
   return (
     <div className="chatInputs">
       <PhotoIcon />
       <GifIcon />
-      <div className="chatTextInput">
-        <input type="text" placeholder="Start a new message" />
+      <div
+        className={
+          isFocus ? "chatTextInput chatTextInputFocus" : "chatTextInput"
+        }
+      >
+        <input
+          type="text"
+          placeholder="Start a new message"
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+        />
         <EmojiIcon />
       </div>
       <SendIcon />
