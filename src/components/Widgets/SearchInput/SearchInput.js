fb7a4f95720ab673a2a489eab40f5f7463d0badd
mucahit-sahin <mucahitsahin786@gmail.com>
2021-03-04 13:49:55 +0300

    SearchInput Focus stili ayarlandı

--- src/components/Widgets/SearchInput/SearchInput.js
+++ src/components/Widgets/SearchInput/SearchInput.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./SearchInput.css";
-
 import SearchIcon from "@material-ui/icons/Search";
+
 function SearchInput({ placeholder }) {
+  const [isFocus, setIsFocus] = React.useState(false);
   return (
-    <div className="widgetsSearch">
-      <SearchIcon className="widgetsSearchIcon" />
+    <div
+      className={isFocus ? "widgetsSearch widgetsSearchFocus" : "widgetsSearch"}
+    >
+      <SearchIcon
+        className={
+          isFocus
+            ? "widgetsSearchIcon widgetsSearchIconFocus"
+            : "widgetsSearchIcon"
+        }
+      />
       <input
         className="widgetsSearchInput"
         type="text"
         placeholder={placeholder}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
       />
     </div>
   );
