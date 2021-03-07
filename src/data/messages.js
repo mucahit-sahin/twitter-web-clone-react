267028e14ea95054832b5e6b299dc171cdb66328
mucahit-sahin <mucahitsahin786@gmail.com>
2021-03-07 16:13:01 +0300

    data oluşturuldu

---
+++ src/data/messages.js
@@ -0,0 +1,46 @@
+const messages = [
+  {
+    fromto: "mucahitsahin6-cnecati",
+    messages: [
+      {
+        message: "merhaba",
+        from: "mucahitsahin6",
+      },
+      {
+        message: "merhaba",
+        from: "cnecati",
+      },
+      {
+        message: "merhaba",
+        from: "cnecati",
+      },
+      {
+        message: "merhaba",
+        from: "mucahitsahin6",
+      },
+    ],
+  },
+  {
+    fromto: "mucahitsahin6-yasin",
+    messages: [
+      {
+        message: "merhaba",
+        from: "mucahitsahin6",
+      },
+      {
+        message: "merhaba",
+        from: "yasin",
+      },
+      {
+        message: "merhaba",
+        from: "yasin",
+      },
+      {
+        message: "merhaba",
+        from: "mucahitsahin6",
+      },
+    ],
+  },
+];
+
+export default messages;
---
+++ src/data/users.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    username: "mucahitsahin6",
+    userimage:
+      "https://avatars2.githubusercontent.com/u/38807255?s=460&u=deb087d587be7f6a4000e4e710ec4d1daa6fde84&v=4",
+    displayName: "Mücahit Şahin",
+  },
+  {
+    username: "cnecati",
+    userimage:
+      "https://pbs.twimg.com/profile_images/1348390204810407937/BmUVaYGD_400x400.jpg",
+    displayName: "Cihat Necati",
+  },
+  {
+    username: "yasin",
+    userimage: "https://i.ytimg.com/vi/eV4fMfIjTZ0/maxresdefault.jpg",
+    displayName: "Yasin",
+  },
+];
+
+export default users;
